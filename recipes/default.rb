26ebf0c33fb04cb1e152eb8d24df167c6741859f
Ushio Shugo <ushio.s@gmail.com>
2014-02-01 23:48:59 +0900

    - homebrew change depends to recommends. - care platform difference.

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,8 +6,12 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+case node["platform_family"]
+when "mac_os_x"
+  include_recipe "homebrew"
 
-package 'dmd' do
-  provider Chef::Provider::Package::Homebrew
-  action :install
+  package 'dmd' do
+    provider Chef::Provider::Package::Homebrew
+    action :install
+  end
 end
